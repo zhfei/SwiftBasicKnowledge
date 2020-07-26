5be26d4e4dacf801dd1031ee837bab8c3bd96f38
zhoufei <1174977961@qq.com>
2020-07-26 11:07:16 +0800

    add: 添加注释

--- SwiftUI_0/SwiftUI_0/ContentView.swift
+++ SwiftUI_0/SwiftUI_0/ContentView.swift
@@ -29,14 +29,19 @@ struct ContentView_Previews: PreviewProvider {
 
 struct Cell: View {
     var body: some View {
+       
         NavigationLink(destination: DetailView()){
             Image("cat").cornerRadius(8)
             VStack(alignment: .leading ) {
+                //属性都可以通过拖拽的方式加入
+                //使用SwiftUI，后面页面实现将非常简单，快捷
                 Text("杭州西湖")
+                    .fontWeight(.thin)
+                    .foregroundColor(Color.red)
+                    .padding([.top, .leading, .bottom], 10.0)
                 Text("西藏-布达拉宫")
                     .bold()
                     .foregroundColor(.accentColor)
-                
             }
         }
     }
