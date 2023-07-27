4390fe049a28a275d4b36d3064b1d2effe9fc51b
zhfei <1174977961@qq.com>
2023-07-27 23:45:57 +0800

    add: 添加详情页

--- ChinaLandmark/ChinaLandmark/LandmarkDetail.swift
+++ ChinaLandmark/ChinaLandmark/LandmarkDetail.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct LandmarkDetail: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("phone7")
+                .resizable()
+                .frame(width: 250, height: 250, alignment: .center)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 10)
+            
+            VStack(alignment: .leading, spacing: 8){
+                Text("天安门").font(.title)
+                HStack{
+                    Text("北京").font(.subheadline)
+                    Spacer()
+                    Text("北京市").font(.subheadline)
+                }
+            }
+        }
+        
     }
 }
 
