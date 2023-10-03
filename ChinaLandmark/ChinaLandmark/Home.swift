a2f87bb709a230e315e25761e64638a56a181ed0
zhfei <1174977961@qq.com>
2023-10-03 12:26:20 +0800

    feat: 完善home页

--- ChinaLandmark/ChinaLandmark/Home.swift
+++ ChinaLandmark/ChinaLandmark/Home.swift
@@ -7,11 +7,41 @@
 
 import SwiftUI
 
+struct HomeCell: View {
+    let cateName: String
+    
+    var body: some View {
+        VStack(alignment:.leading){
+            Text(cateName)
+                .font(.headline)
+            let list = categories[cateName]
+            ScrollView.init(.horizontal, showsIndicators: false) {
+                HStack{
+                    ForEach(Range.init(0...10)) { index in
+                        let item = list![index%2]
+                        VStack(alignment: .leading){
+                            Image(item.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(height:150)
+                                .clipped()
+                                .cornerRadius(5)
+                            Text(item.name)
+                                .font(.caption)
+                        }
+                    }
+                }
+            }
+        }
+        .padding(.trailing,12)
+        .padding([.bottom,.top],8)
+    }
+}
+
 struct Home: View {
     var categories: [String: [Landmark]] {
         Dictionary(grouping: landmarks, by: {$0.category})
     }
-    
     
     var body: some View {
         NavigationView {
@@ -25,28 +55,17 @@ struct Home: View {
                 
                 ForEach(Range(NSRange(location: 0, length: categories.keys.count))!) { index in
                     let cateName = categories.keys.sorted()[index]
-                    Text(cateName)
-                    
-                    let list = categories[cateName]
-                    ScrollView.init(.horizontal) {
-                        ForEach(list!) { item in
-                            Image(item.imageName)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(height:150)
-                                .clipped()
-                        }
-                        
-                    }
-                    
+                    HomeCell(cateName: cateName)
                 }
-                
-                
+                .listRowInsets(EdgeInsets())
+                .listRowSeparator(Visibility.hidden)
                 
                 NavigationLink(destination: LandmarkList()) {
                     Text("查看所有地标")
                 }
-            }.navigationTitle("精选")
+                .listRowInsets(EdgeInsets.init())
+            }
+            .navigationTitle("精选")
         }
         
     }
