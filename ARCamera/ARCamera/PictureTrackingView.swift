49f21136ccc217328d723d4bded6c5fd32c93081
zhfei <1174977961@qq.com>
2023-12-21 23:18:46 +0800

    feat： AR相机，使用本地图像库进行图片追踪

---
+++ ARCamera/ARCamera/PictureTrackingView.swift
@@ -0,0 +1,63 @@
+//
+//  PictureTrackingView.swift
+//  ARCamera
+//
+//  Created by zhoufei on 2023/12/21.
+//
+
+import SwiftUI
+import RealityKit
+import ARKit
+
+struct PictureTrackingView: View {
+    var body: some View {
+        PictureTrackingViewContainer()
+    }
+}
+
+var theARView: ARView?
+
+struct PictureTrackingViewContainer: UIViewRepresentable {
+    func makeUIView(context: Context) -> ARView {
+        let arView = ARView(frame: .zero)
+        let config = ARImageTrackingConfiguration()
+        guard let trackedImageLib = ARReferenceImage.referenceImages(inGroupNamed: "ARImageResources", bundle: Bundle.main) else {
+            fatalError("无法加载图像参考库")
+        }
+        
+        config.trackingImages = trackedImageLib
+        config.maximumNumberOfTrackedImages = 2
+        arView.session.run(config, options: [])
+        arView.session.delegate = arView
+        theARView = arView
+        return arView
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+}
+
+extension ARView: ARSessionDelegate {
+    public func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        guard let imageAnchor = anchors.first as? ARImageAnchor else {
+            return
+        }
+        
+        let refrenceImageName = imageAnchor.referenceImage.name ?? "toy_biplane"
+        DispatchQueue.main.async {
+            do {
+                let myModelEntity = try Entity.loadModel(named: refrenceImageName)
+                let imageAnchorEntity = AnchorEntity(anchor: imageAnchor)
+                imageAnchorEntity.addChild(myModelEntity)
+                self.scene.addAnchor(imageAnchorEntity)
+            } catch {
+                print("加载模型失败",error.localizedDescription)
+            }
+        }
+        
+    }
+}
+
+
+#Preview {
+    PictureTrackingView()
+}
